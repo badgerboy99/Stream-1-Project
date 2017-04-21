c590a8a11b34523ddf9ebb18d771f11787d7664f
Chris <releasethebadgers@yahoo.co.uk>
2017-04-21 14:21:48 +0100

    Developing footer

--- my_script.js
+++ my_script.js
@@ -9,12 +9,23 @@ $(document).ready(function(){
  	}); // closing tags menu reveal click
 
 
- 	$("nav ul li").bind("click touchstart", function() {
- 		$(this).addClass("highlight-nav");                   //adds highlight to li
-	}); // closing tags menu item highlight click
+ 	//$("nav ul li").bind("click touchstart", function() {
+ 		//$(this).addClass("highlight-nav");  //adds highlight to li
+ 		//$("nav ul li".("highlight-nav").removeClass("highlight-nav");
+	//}); // closing tags menu item highlight click
 
  	$("nav li.drop").bind("click touchstart", function() {   //reveals sub-menu items
- 		$(this).next("nav li.supp").slideDown(1000);
- 	});
+ 		$(this).children().slideDown(1000);
+ 	
+	});
 
-});  //closing tags for "document ready"
+ 	$("nav ul li").click(function(){
+     	
+    	$(this).addClass("highlight-nav");
+    	$("nav ul li").removeClass("highlight-nav");
+    	
+    });
+
+ 
+
+  });//closing tags for "document ready
